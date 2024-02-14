d941d01f75c92c4ebb216b318f513dc1587cfab1
adiboy6 <aditya.reddy.97@gmail.com>
2024-02-14 12:19:23 -0500

    final updates with CCA

--- students/generate_graphs.py
+++ students/generate_graphs.py
@@ -2,32 +2,50 @@ import os
 import matplotlib.pyplot as plt
 
 # Parse the log file
-for filename in os.listdir(os.getcwd()):
+filepath = os.getcwd()+'/metrics/'
+for filename in os.listdir(filepath+'logs/'):
     times = []
     cwnd_sizes = []
     throughputs = []
+    data = {}
     if filename.startswith('log-'):
-        print(filename)
-        with open(filename, 'r') as f:
+        with open(filepath+'logs/'+filename, 'r') as f:
             for line in f:
                 parts = line.split()
-                if parts[1] == 'data]':
-                    times.append(float(parts[2]))
-                    cwnd_sizes.append(float(parts[3]))
-                elif parts[1] == 'ack]':
-                    throughputs.append(float(parts[3]))
+                port = parts[1]
+                if port not in data:
+                    data[port] = {'times': [], 'cwnd_sizes': [], 'throughputs': []}
+                if parts[3] == 'data]':
+                    data[port]['times'].append(float(parts[4]))
+                    data[port]['cwnd_sizes'].append(float(parts[5]))
+                elif parts[3] == 'ack]':
+                    data[port]['throughputs'].append(float(parts[5]))
                 
             # Generate the graphs
             try:
                 plt.figure()
-                plt.plot(times, cwnd_sizes)
+                for port, values in data.items():
+                    plt.plot(values['times'], values['cwnd_sizes'], label=f'Port {port}')
                 plt.title('Congestion Window Size Over Time')
                 plt.xlabel('Time (s)')
                 plt.ylabel('Congestion Window Size (packets)')
-                plt.savefig('cwnd'+filename[:-4]+'.png')
+                plt.legend()
+                plt.savefig(filepath+'graphs/'+'cwnd-'+filename[:-4]+'.png')
             except Exception as e:
                 print(e)
                 print("error in figure 1!")
+            
+            # for port, values in data.items():
+            #     try:
+            #         plt.figure()
+            #         plt.plot(values['times'], values['cwnd_sizes'])
+            #         plt.title(f'Congestion Window Size Over Time (Port {port})')
+            #         plt.xlabel('Time (s)')
+            #         plt.ylabel('Congestion Window Size (packets)')
+            #         plt.savefig(filepath+'graphs/'+'cwnd-'+filename[:-4]+'-'+port+'.png')
+            #     except Exception as e:
+            #         print(e)
+            #         print("error in figure 1!")
 
             # try:
             #     plt.figure()
